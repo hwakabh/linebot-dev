ea1e4cdf9c72386445db0c820d3011aeebb604ae
hwakabh <hrykwkbys1024@gmail.com>
2021-02-18 19:37:38 +0900

    #16 Optimized sources with using Cloud Functions.

--- index.js
+++ index.js
@@ -1,91 +1,125 @@
-// Importing modules
-const server = require("express")();
-const line = require("@line/bot-sdk");
-const redis = require("redis");
-const url = require("url");
+// Determine the day of week, and define message of the day
+// Mon+Thr -> 可燃ゴミ
+// Sat -> 不燃ゴミ(第1/3週のみ)
+// Sat -> ペットボトル(第2/4週のみ)
+// Fri -> ビン/カン/段ボール
+function get_day_index(date_checked) {
+  try {
+      date_checked.getDate();
+      var d = date_checked;
+  } catch (e) {
+      var d = new Date(date_checked[0],date_checked[1],date_checked[2]);
+  }
 
-
-// Instanciate LINE Client
-// Notes: Access token and channel secret should be used as environmental variables
-const line_conf = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET
+  var c = 1;
+  for (var i = d.getDate(); i>7; i-=7) {
+      c++;
+  }
+  // For debugging
+  const days = ["SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT" ];
+  console.log(`>>> Today is ${c}-th ${days[d.getDay()]} in the month`);
+  return c;
 };
-const bot = new line.Client(line_conf);
-// Instanciate Redis client
-if (process.env.REDISTOGO_URL) {
-    var redis_target = url.parse(process.env.REDISTOGO_URL);
-    var redisClient = redis.createClient(redis_target.port, redis_target.hostname);
-    redisClient.auth(redis_target.auth.split(":")[1]);
-} else {
-    var redisClient = redis.createClient();
-}
-redisClient.on("error", function (err) {
-    console.log("Failed to connect redis-server: " + err);
-})
 
 
-const defaultMessage = `
-可燃ゴミ: 月曜日 と 木曜日
-不燃ゴミ: 第1/3土曜日
-ペットボトル: 第2/4土曜日
-ビン/カン/段ボール: 金曜日
-`;
+function set_daily_message() {
+  var msg = "";
+  var today = new Date;
+  var day_of_today = today.getDay();
 
-// Web Server configurations
-server.listen(process.env.PORT || 3000);
+  switch(day_of_today) {
+      case 1:
+          msg = "今日は「可燃ゴミ」のゴミ出しの日です";
+          break;
+      case 4:
+          msg = "今日は「可燃ゴミ」のゴミ出しの日です";
+          break;
+      case 5:
+          msg = "今日は「ビン・カン・段ボール」のゴミ出しの日です";
+          break;
+      case 6:
+          if (get_day_index(today) == 1 || get_day_index(today) == 3) {
+              msg = "今日は「不燃ゴミ」のゴミ出しの日です";
+          } else if (get_day_index(today) == 2 || get_day_index(today) == 4) {
+              msg = "今日は「ペットボトル」のゴミ出しの日です";
+          } else {
+              msg = "";
+          }
+          break;
+      default:
+          msg = "";
+          break
+  }
+  return msg;
+};
 
-// Express Router Configurations
-server.post('/bot/webhook', line.middleware(line_conf), (req, res, next) => {
-    res.sendStatus(200);
 
-    console.log(">>>>>>>>> Reponse from User: ");
-    console.log(req.body);
-    console.log("");
+// Main functions
+exports.makeNotices = (req, res) => {
+  // Get Credentials securely from environmental variables
+  var line_access_token = process.env.LINE_ACCESS_TOKEN;
+  if (process.env.TZ == 'undefined') {
+      process.env.TZ = 'Asia/Tokyo';
+  }
 
-    if (req.body.events[0].type === 'follow') {
-        var addedUserId = req.body.events[0].source.userId;
-        console.log(`>>>>>>>>> Somebody added me, userId: ${addedUserId}`);
-        redisClient.set(addedUserId, addedUserId);
-        console.log("");
-    }
-    if (req.body.events[0].type === 'unfollow') {
-        var blockedUserId = req.body.events[0].source.userId;
-        console.log(`>>>>>>>>> Somebody blocked me, userId: ${blockedUserId}`);
-        redisClient.del(blockedUserId);
-        console.log("");
-    }
+  // Import & Setup request
+  var request = require("request");
+  var line_api_endpoint = "https://api.line.me/v2/bot/message/push";
+  var headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${line_access_token}`,
+  };
 
-    console.log(">>> DEBUG: UserIds in redis cache: ");
-    redisClient.keys('*', function (err, keys) {
-        keys.forEach(function (fid) {
-            console.log(fid);
-        });
-    });
-    console.log("");
+  // Import & Setup node-redis
+  const redis = require("redis");
+  const url = require("url");
+  // Instanciate Redis client
+  if (process.env.REDISTOGO_URL) {
+      var redis_target = url.parse(process.env.REDISTOGO_URL);
+      var redisClient = redis.createClient(redis_target.port, redis_target.hostname);
+      redisClient.auth(redis_target.auth.split(":")[1]);
+  } else {
+      var redisClient = redis.createClient();
+  }
+  redisClient.on("error", function (err) {
+      console.log("Failed to connect redis-server: " + err);
+      res.status(500).send("Failed to connect redis-server...")
+  })
 
-    // For interactive messages and define defalt messages
-    let events_processed = [];
-    req.body.events.forEach((event) => {
-        if (event.type == "message" && event.message.type == "text") {
-            if (event.message.text == "help") {
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "HELP MESSAGE HERE"
-                }));
-            } else {
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                  type: "text",
-                  text: defaultMessage
-                }));
-            }
-        }
-    });
+  // Determine if make notifications or not
+  redisClient.keys('*', function (err, keys) {
+      keys.forEach(function (fid) {
+          // Set message of the day
+          var text = set_daily_message();
+          // Send message to user on specific day
+          if (text !== "") {
+              var message = {
+                type: "text",
+                text: set_daily_message()
+              };
 
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`>>>>>>>>> ${response.length} event(s) processed.`);
-            console.log("");
-        }
-    )
-});
+              console.log(`>>> Trying to send message to user: ${fid}`);
+              console.log(message);
+              // Build HTTP request body
+              var reqbody = {
+                  "to": fid,
+                  "messages": [message]
+              };
+              var options = {
+                  uri: line_api_endpoint,
+                  method: "POST",
+                  headers: headers,
+                  json: reqbody,
+                  rejectUnauthorized: false,
+              };
+              // Calling API with request module
+              request.post(options, function(error, response, body){});
+              res.status(200).send("Sucessfully noticed to users.");
+
+          } else {
+              res.status(200).send("Nothing to do for today.");
+          }
+
+      });
+  });
+};
