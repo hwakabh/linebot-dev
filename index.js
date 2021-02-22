0564407cd5cb11c0a4a5885e647342a0ebbeb189
hwakabh <hrykwkbys1024@gmail.com>
2021-02-22 20:01:45 +0900

    Modified variable types which should use as const.

--- index.js
+++ index.js
@@ -59,14 +59,14 @@ function set_daily_message() {
 // Main functions
 exports.makeNotices = (req, res) => {
   // Get Credentials securely from environmental variables
-  var line_access_token = process.env.LINE_ACCESS_TOKEN;
+  const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
 
   // Import & Setup request
-  var request = require("request");
-  var line_api_endpoint = "https://api.line.me/v2/bot/message/push";
+  const request = require("request");
+  const line_api_endpoint = "https://api.line.me/v2/bot/message/push";
   var headers = {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${line_access_token}`,
+      "Authorization": `Bearer ${LINE_ACCESS_TOKEN}`,
   };
 
   // Import & Setup node-redis
